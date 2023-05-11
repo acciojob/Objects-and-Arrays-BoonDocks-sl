ef2d8c1abb4fff4be6cc6a62fdef0239800ea7e5
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-05-11 22:48:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+// create a reference to players
+let team = players;
+
+// create a copy of players
+let team1 = [...players];
+
+// create a copy of person
+let cap1 = {...person};
+
 window.players = players;
 window.person = person;
 window.team = team;
